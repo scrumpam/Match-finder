e99ba97cce7a368ae57bafb5cb7c6a414abbe19e
unknown <pamprojekt@gmail.com>
2012-06-17 14:27:20 +0200

    dodalem paczke googlemaps

---
+++ src/dmcs/matchfinder/googlemaps/GoogleMapActivity.java
@@ -0,0 +1,14 @@
+package dmcs.matchfinder.googlemaps;
+
+import matchfinder.ui.activites.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoogleMapActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main_layout);
+	}
+}
